48f6eb6ca6f5db3ba0fa99332921387c7dde588d
Aquila <aquilahenrique.silva@gmail.com>
2021-10-12 12:42:07 -0300

    added react native navigation to the project

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const Home = () => {
     return (
         <View style={styles.container}>
-        <Text>Esta é a home!!!</Text>
+        <Text>Esta é a home!!!!!!!</Text>
         </View>
 
     );
