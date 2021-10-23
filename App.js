320b187096372d6a1ee1d2ec14506cc14f3645ee
Artur Fidelis <38798540+fidelis33@users.noreply.github.com>
2021-10-22 22:51:10 -0300

    Add files via upload
    import React, { useState } from "react";
    import {  View, Picker, StyleSheet, Text, TextInput, Button, Alert, Image } from 'react-native';
    
    export default function TelaFiltraServices({ navigation }) {
    
      const [selectedValue, setSelectedValue] = useState("");  
      return (
        <View style={styles.container}>
           <Text> Localizacao</Text>
           <TextInput
            autoCapitalize={'none'}
            style={styles.field}
          />
           <Text> Orcamento (R$)</Text>
           <TextInput
            autoCapitalize={'none'}
            style={styles.field}
          />
          <View style={{flexDirection:"row"}}>
              <Text> De</Text>
              <TextInput style={styles.field2} />
              <Text> Ate</Text>
              <TextInput style={styles.field2} />
          </View>
          <Text> Modalidade</Text>
          <Picker
            selectedValue={selectedValue}
            style={styles.field}
            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
          >
            <Picker.Item label="Selecione" value="" />
            <Picker.Item label="Eletricista" value="eletricista" />
            <Picker.Item label="Hidraulico" value="hidraulico" />
            <Picker.Item label="Pedreiro" value="pedreiro" />
            <Picker.Item label="Marceneiro" value="marceneiro" />
          </Picker>
          <View style={styles.fixToText}>
            <Button
              title="Filtrar"
              onPress={() => navigation.navigate('Cadastro')}
              color="#F7C302"
            />
           
          </View>
        </View>
      );
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        flexDirection: 'column',
        justifyContent: 'center',
        padding: 45,
      },
      field: {
        borderBottomWidth: 2,
        borderColor: '#F7C302',
        paddingBottom: 5,
        marginBottom: 10,
      }, 
       field2: {
        borderBottomWidth: 2,
        borderColor: '#F7C302',
        marginBottom: 10,
        width: 95,
        
      }, 
    });

--- App.js
+++ App.js
@@ -1,53 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SplashScreen from './components/splashScreen';
-import Home from './screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TelaLogin from './screens/TelaLogin';
-import TelaCadastro from './screens/TelaCadastro';
+import React, { useState } from "react";
+import {  View, Picker, StyleSheet, Text, TextInput, Button, Alert, Image } from 'react-native';
 
-export default function App() {
-  const Stack = createNativeStackNavigator();
-  const [splash, setSplash] = useState(true);
-  useEffect(() => {
-    setTimeout(() => setSplash(false), 3000);
-  });
+export default function TelaFiltraServices({ navigation }) {
 
-  const HeaderOptions = {
-    headerStyle: {
-      backgroundColor: '#F7C302',
-    },
-    headerTitleAlign: 'center',
-    headerTitleStyle: { fontWeight: 'bold' },
-  };
-
+  const [selectedValue, setSelectedValue] = useState("");  
   return (
-    <>
-      {splash ? (
-        <SplashScreen />
-      ) : (
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Faça o Login"
-              component={TelaLogin}
-              options={HeaderOptions}
-            />
-            <Stack.Screen
-              name="21Worker"
-              component={Home}
-              options={HeaderOptions}
-            />
-            <Stack.Screen
-              name="Cadastro"
-              component={TelaCadastro}
-              options={HeaderOptions}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      )}
-    </>
+    <View style={styles.container}>
+       <Text> Localizacao</Text>
+       <TextInput
+        autoCapitalize={'none'}
+        style={styles.field}
+      />
+       <Text> Orcamento (R$)</Text>
+       <TextInput
+        autoCapitalize={'none'}
+        style={styles.field}
+      />
+      <View style={{flexDirection:"row"}}>
+          <Text> De</Text>
+          <TextInput style={styles.field2} />
+          <Text> Ate</Text>
+          <TextInput style={styles.field2} />
+      </View>
+      <Text> Modalidade</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={styles.field}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Selecione" value="" />
+        <Picker.Item label="Eletricista" value="eletricista" />
+        <Picker.Item label="Hidraulico" value="hidraulico" />
+        <Picker.Item label="Pedreiro" value="pedreiro" />
+        <Picker.Item label="Marceneiro" value="marceneiro" />
+      </Picker>
+      <View style={styles.fixToText}>
+        <Button
+          title="Filtrar"
+          onPress={() => navigation.navigate('Cadastro')}
+          color="#F7C302"
+        />
+       
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 45,
+  },
+  field: {
+    borderBottomWidth: 2,
+    borderColor: '#F7C302',
+    paddingBottom: 5,
+    marginBottom: 10,
+  }, 
+   field2: {
+    borderBottomWidth: 2,
+    borderColor: '#F7C302',
+    marginBottom: 10,
+    width: 95,
+    
+  }, 
+});
