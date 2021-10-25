eb7453e9f45c014e2f1cb18f6eafccd276bd8ea2
Aquila Henrique da Silva Faria <53267575+henrikeSilva93@users.noreply.github.com>
2021-10-25 18:49:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,58 +1,36 @@
 import React, { useState } from "react";
-import {  View, Picker, StyleSheet, Text, TextInput, Button, Alert, Image } from 'react-native';
+import {  View, StyleSheet, Text } from 'react-native';
 
 export default function TelaCadastrarServices({ navigation }) {
 
   const [selectedValue, setSelectedValue] = useState("");  
   return (
-    <View style={styles.container}>
-       <Text> Titulo</Text>
-       <TextInput
-        autoCapitalize={'none'}
-        style={styles.field}
-      />
-       <Text> Endereço </Text>
-       <TextInput
-        autoCapitalize={'none'}
-        style={styles.field}  
-      />
-       <Text> Descrição</Text>
-       <TextInput
-        autoCapitalize={'none'}
-        style={styles.field}
-      />
-       <Text> Orçamento (R$) </Text>
-       <TextInput
-        autoCapitalize={'none'}
-        style={styles.field}
-      />
-      <View style={{flexDirection:"row"}}>
-          <Text> De</Text>
-          <TextInput style={styles.field2} />
-          <Text> Ate</Text>
-          <TextInput style={styles.field2} />
-      </View>
-      <Text> Modalidade</Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={styles.field}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Selecione" value="" />
-        <Picker.Item label="Eletricista" value="eletricista" />
-        <Picker.Item label="Hidraulico" value="hidraulico" />
-        <Picker.Item label="Pedreiro" value="pedreiro" />
-        <Picker.Item label="Marceneiro" value="marceneiro" />
-      </Picker>
-      <View style={styles.fixToText}>
-        <Button
-          title="Cadastrar Serviços"
-          onPress={() => navigation.navigate('Cadastro')}
-          color="#F7C302"
-        />
-       
-      </View>
-    </View>
+    <>
+      {splash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Faça o Login"
+              component={TelaLogin}
+              options={HeaderOptions}
+            />
+            <Stack.Screen
+              name="home"
+              component={Home}
+              options={HeaderOptions}
+            />
+            <Stack.Screen
+              name="Cadastro"
+              component={TelaCadastro}
+              options={HeaderOptions}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+    </>
+>>>>>>> main
   );
 }
 
@@ -62,18 +40,5 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     padding: 45,
-  },
-  field: {
-    borderBottomWidth: 2,
-    borderColor: '#F7C302',
-    paddingBottom: 5,
-    marginBottom: 10,
-  }, 
-   field2: {
-    borderBottomWidth: 2,
-    borderColor: '#F7C302',
-    marginBottom: 10,
-    width: 95,
-    
-  }, 
+  }
 });
